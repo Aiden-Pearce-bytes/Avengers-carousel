f460d1dc034f9661abcf3f09c00f283a3b70dabb
AidenPearce <dineshkumarr0597@gmail.com>
2023-05-01 21:41:28 +0530

    Avengers

---
+++ climate.jsx
@@ -0,0 +1,53 @@
+export const climate = [
+    {
+        img: "https://w0.peakpx.com/wallpaper/477/401/HD-wallpaper-thor-in-avengers-endgame.jpg",
+        place: "Germany",
+        name: "Fall"
+    },
+    {
+        img: "https://w0.peakpx.com/wallpaper/359/806/HD-wallpaper-black-panther-avengers-marvel-panther-wakanda.jpg",
+        place: "Gold Creek Pond",
+        name: "Winter"
+    },
+    {
+        img: "https://w0.peakpx.com/wallpaper/477/401/HD-wallpaper-thor-in-avengers-endgame.jpg",
+        place: "Germany",
+        name: "Autumn"
+    },
+    {
+        img: "https://images.unsplash.com/photo-1543683840-c66117bdb1f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        place: "Germany",
+        name: "Spring"
+    },
+    {
+        img: "https://images.unsplash.com/photo-1543683840-c66117bdb1f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        place: "Germany",
+        name: "Summer"
+    },
+    {
+        img: "https://images.unsplash.com/photo-1543683840-c66117bdb1f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        place: "Germany",
+        name: "Forest"
+    },
+    {
+        img: "https://images.unsplash.com/photo-1543683840-c66117bdb1f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        place: "Germany",
+        name: "Mountains"
+    },
+    {
+        img: "https://images.unsplash.com/photo-1543683840-c66117bdb1f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        place: "Germany",
+        name: "Mountains"
+    },
+    {
+        img: "https://images.unsplash.com/photo-1543683840-c66117bdb1f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        place: "Germany",
+        name: "Mountains"
+    },
+    {
+        img: "https://images.unsplash.com/photo-1543683840-c66117bdb1f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        place: "Germany",
+        name: "Mountains"
+    },
+
+]
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,82 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState, useRef } from "react";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { climate } from "@/climate";
+import classNames from "classnames";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [activeItem, setActiveItem] = useState(3);
+  const wrapperRef = useRef(0);
+  const timeOutRef = useRef(0);
+
+  useEffect(() => {
+    if (!wrapperRef.current) return;
+    // To clearout the unwanted Timeout functions
+    if (timeOutRef.current) {
+      clearTimeout(timeOutRef.current);
+    }
+
+    wrapperRef.current.style.setProperty(
+      "--transition",
+      "600ms cubic-bezier(0.22,0.61, 0.36, 1)"
+    );
+    timeOutRef.current = setTimeout(() => {
+      wrapperRef.current?.style.removeProperty("--transition");
+    }, 900);
+
+    // CleanUp Function
+    return () => {
+      if (timeOutRef.current) {
+        clearTimeout(timeOutRef.current);
+      }
+    };
+  }, [activeItem]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex bg-white h-screen w-full items-center justify-center">
+      <div className="w-[1200px] max-w-full">
+        <ul ref={wrapperRef} className="group flex flex-col md:flex-row md:h-[640px] gap-4 md:gap-[1.5%]">
+          {climate.map((climate, index) => (
+            <li
+              onClick={() => setActiveItem(index)}
+              aria-current={activeItem === index}
+              className={classNames(
+                "relative cursor-pointer md:w-[8%] md:first:w-[1%] md:last:w-[1%] md:[&[aria-current='true']]:w-[48%]",
+                "md:[transition:width_var(--transition,200ms_ease-in)]",
+                "before:hidden md:before-block before:bg-white before:absolute before:bottom-0 before:left-[-10px] before:top-0 before:right-[-10px]",
+                "md:hover:w-[12%] md:[&:not(:hover),&:not(:first), &:not(:last)]:group-hover:w-[7%]",
+                "first:pointer-events-none last:pointer-events-none md:[&_img]:first:opacity-0 md:[&_img]:last:opacity-0"
+              )}
+              key={climate.name}
+            >
+              <div className="relative w-full h-full overflow-hidden rounded-2xl bg-gray-300">
+                <img
+                  className="absolute right-0 max-w-none w-24 h-auto md:h-[640px] md:w-[590px] md:left-1/2 top-1/2 md:-translate-x-1/2 -translate-y-1/2 object-cover"
+                  src={climate.img}
+                  alt={climate.name}
+                  width="590px"
+                  height="640px"
+                />
+                <div
+                  className={classNames(
+                    "inset-0 opacity-25 duration-300 before:absolute before:bottom-0 before:left-[-546px] before:right-0 before:top-[-148px] before:z-10 before:bg-sky-200  after:bottom-[28px] after:left-0 after:right-[-434px] after:top-0 after:z-10 after:bg-sky-400 md:absolute md:transition-opacity",
+                    activeItem === index ? "md:opacity-25" : "md:opacity-0"
+                  )}
+                />
+                <div className={classNames(
+                  "p-4 md:absolute md:p-0 left-8 top-8 w-[590px] transition-[transform,opacity]",
+                  activeItem === index ? "md:translate-x-0 md:opacity-100" : "md:translate-x-4 md:opacity-0"                
+                )}>
+                  <p className="text-sm md:text-lg uppercase text-purple-500">{climate.place}</p>
+                  <p className="text-lg md:text-4xl font-bold">{climate.name}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
